7ad1145bf005758b7c72f34fbbba539bbf423221
Arihant Singh Verma <78214117+arihant30@users.noreply.github.com>
2021-07-11 21:46:47 +0530

    Create 19 Mirror Tree.java

---
+++ 19 Mirror Tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        if(node==null){
+            return;
+        }
+        else{
+            mirror(node.left);
+            mirror(node.right);
+            Node temp=node.left;
+            node.left=node.right;
+            node.right=temp;
+        }
+    }
+}
