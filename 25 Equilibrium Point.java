4ba83c3be6e71d84f91fb1504e5b517701fd0027
Arihant Singh Verma <78214117+arihant30@users.noreply.github.com>
2021-07-17 22:53:06 +0530

    Update and rename 25 Equilibrium Point to 25 Equilibrium Point.java

--- 25 Equilibrium Point.java
+++ 25 Equilibrium Point.java
@@ -20,3 +20,18 @@ class Solution {
         return -1;
     }
 }
+
+/*
+public static int equilibriumPoint(long arr[], int n) {
+        long sum=0;
+        long leftrightsum=0;
+        for(long i:arr){
+            sum+=i;
+        }
+        for(int i=0;i<n;i++){
+            if(leftrightsum==(sum-arr[i])*0.5) return i+1;
+            if(leftrightsum>(sum-arr[i]*0.5)) return -1;
+            leftrightsum+=arr[i];
+        }
+        return -1;
+    } */
